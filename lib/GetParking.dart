41c1ce7050b7b7dfb9faefcdb1bdbb46e717b045
Chetan Nager <chetannager64@gmail.com>
2020-03-08 13:26:10 +0530

    first commit

---
+++ lib/GetParking.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GetParking extends StatefulWidget {
+  @override
+  _GetParkingState createState() => _GetParkingState();
+}
+
+class _GetParkingState extends State<GetParking> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('data'),
+    );
+  }
+}
